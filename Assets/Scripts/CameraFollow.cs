8ec987541d094f14f0a67824bf6fdd5c0852c665
Dungstenium <dungstenium@outlook.com>
2020-01-30 22:58:23 +0100

    Player can move and camera can look around

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class CameraFollow : MonoBehaviour
+{
+    const string mouseXInputName = "Mouse X";
+    const string mouseYInputName = "Mouse Y";
+    [SerializeField] float mouseSensitivity;
+    float xAxisClamp;
+    Transform playerBodyTransform;
+    void Awake()
+    {
+        LockCursorToScreenCenter();
+        xAxisClamp = 0.0f;
+
+        if (playerBodyTransform == null)
+        {
+            playerBodyTransform = GetComponentInParent<PlayerController>().transform;
+        }
+    }
+    void Update()
+    {
+        CameraRotation();
+    }
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        if (xAxisClamp > 80.0f)
+        {
+            xAxisClamp = 80.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValues(270.0f);
+        }
+        else if (xAxisClamp < -70.0f)
+        {
+            xAxisClamp = -70.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValues(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBodyTransform.Rotate(Vector3.up * mouseX);
+    }
+    void ClampXAxisRotationToValues(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+    void LockCursorToScreenCenter()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    [SerializeField] float speed = 0.2f;
+    Vector3 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        moveDirection = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputManager();
+        moveDirection = Vector3.ClampMagnitude(moveDirection, 1);
+        rigidBody.velocity = moveDirection.normalized * speed;
+        //transform.SetPositionAndRotation(transform.position, new Quaternion(transform.rotation.x, Camera.main.transform.rotation.y, transform.rotation.z,transform.rotation.w));
+        //transform.rotation = new Quaternion(transform.rotation.x, Camera.main.transform.rotation.y, transform.rotation.z, transform.rotation.w);
+    }
+    void InputManager()
+    {
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            MoveLeft();
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            MoveRight();
+        }
+        else if (Input.GetAxis("Horizontal") == 0)
+        {
+            StopMovingSides();
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            MoveForward();
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            MoveBackward();
+        }
+        else if (Input.GetAxis("Vertical") == 0)
+        {
+            StopMovingForward();
+        }
+    }
+    private void StopMovingForward()
+    {
+        moveDirection.z = Mathf.Lerp(moveDirection.z, 0.0f, 0.4f);
+    }
+    private void StopMovingSides()
+    {
+        moveDirection.x = Mathf.Lerp(moveDirection.x, 0.0f, 0.4f);
+    }
+    private void MoveForward()
+    {
+        moveDirection += transform.forward;
+    }
+    private void MoveBackward()
+    {
+        moveDirection += -transform.forward;
+    }
+    private void MoveRight()
+    {
+        moveDirection += transform.right;
+    }
+    private void MoveLeft()
+    {
+        moveDirection += -transform.right;
+    }
+}
